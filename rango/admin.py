116ab436048ee38c1df779efef68db870fb38a34
ataide <scorpionspfc@gmail.com>
2018-09-22 23:59:40 -0300

    new

--- rango/admin.py
+++ rango/admin.py
@@ -3,7 +3,7 @@ from rango.models import Category, Page
 # Register your models here.
 
 class PageAdmin(admin.ModelAdmin):
-    list_display = ('title', 'category', 'url')
+    list_display = ('title', 'category', 'url', 'views')
 
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
